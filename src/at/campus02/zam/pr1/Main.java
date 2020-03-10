2a55e766b76c24a6d99805bc721c6dc084d67263
e.jordamovic-kuric <emina.jordamovich@outlook.com>
2020-03-10 13:41:04 +0100

    new method added

--- src/at/campus02/zam/pr1/Main.java
+++ src/at/campus02/zam/pr1/Main.java
@@ -2,17 +2,14 @@ package at.campus02.zam.pr1;
 
 public class Main {
 
-    public static double wieWeitSpazieren (double gewicht, int letztesMal, boolean vertraegtSich) {
-        if (gewicht <= 1 && vertraegtSich){
+   /* public static double wieWeitSpazieren(double gewicht, int letztesMal, boolean vertraegtSich) {
+        if (gewicht <= 1 && vertraegtSich) {
             return 2;
-        }
-        else if (gewicht < 1 && vertraegtSich){
+        } else if (gewicht < 1 && vertraegtSich) {
             return 4;
-        }
-        else if (gewicht > 15 || letztesMal > 24) {
+        } else if (gewicht > 15 || letztesMal > 24) {
             return 8;
-        }
-        else {
+        } else {
             return 5;
         }
     }
@@ -23,11 +20,11 @@ public class Main {
 
         //Test fuer 1. Fall
         System.out.print("Hund soll: ");
-        System.out.println(wieWeitSpazieren(0.8, 3, false ) + " km spazieren gehen");
+        System.out.println(wieWeitSpazieren(0.8, 3, false) + " km spazieren gehen");
 
         //Test fuer 2. Fall
         System.out.print("Hund soll: ");
-        System.out.println(wieWeitSpazieren(0.5, 8, true ) + " km spazieren gehen");
+        System.out.println(wieWeitSpazieren(0.5, 8, true) + " km spazieren gehen");
 
         //Test fuer 3. Fall
         System.out.print("Hund soll: ");
@@ -38,12 +35,52 @@ public class Main {
         System.out.println(wieWeitSpazieren(0.7, 6, false) + " km spazieren gehen");
 
 
-    }
+    }*/
+
+   //int [] kuscheleinheit = new int[] {12, 22, 32};
+
+   public static boolean kuschelBedarf(int[] kuscheleinheiten) {
+       if (kuscheleinheiten[0] + kuscheleinheiten[1] + kuscheleinheiten[2] > 60){
+           return false;
+       } else if (kuscheleinheiten[0] < kuscheleinheiten[1] && kuscheleinheiten[1] < kuscheleinheiten[2]) {
+           return true;
+       } else if (kuscheleinheiten[1] > 25) {
+           return false;
+       } else {
+           return true;
+       }
+
+   }
+
+   public static void testKuschelBedarf() {
+       System.out.println("Test fuer Methode kuschelBedarf...");
+
+       //Test fuer 1. Fall
+       System.out.print("Der Hund soll kuscheln werden: ");
+       System.out.println(kuschelBedarf(new int[]{12, 22, 32}));
+
+       //Test fuer 2. Fall
+       System.out.print("Der Hund soll kuscheln werden: ");
+       System.out.println(kuschelBedarf(new int[]{10, 20, 30}));
+
+       //Test fuer 3. Fall
+       System.out.print("Der Hund soll kuscheln werden: ");
+       System.out.println(kuschelBedarf(new int[]{12, 26, 32}));
+
+       //Test fuer 4. Fall
+       System.out.print("Der Hund soll kuscheln werden: ");
+       System.out.println(kuschelBedarf(new int[]{12, 22, 32}));
+
+   }
+
 
 
     public static void main(String[] args) {
-	// write your code here
-        testWieWeitSpazieren();
+        // write your code here
+        //testWieWeitSpazieren();
+        testKuschelBedarf();
+
+
 
     }
 }
